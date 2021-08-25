793f8ffed6ea113a35e6e9f95218c2571c629185
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-08-25 13:07:47 +0500

    Completed step25

---
+++ step25/EthreumAccount.ts
@@ -0,0 +1,69 @@
+import Web3 from "web3";
+import { Transaction, TxData } from "ethereumjs-tx";
+
+export class EthereumAccount {
+  private web3: Web3;
+  private address: string;
+  private privateKey?: ReturnType<typeof Buffer.from>;
+  private remoteNode =
+    "https://ropsten.infura.io/v3/aee7d40b94544f8e8cb7f1e41e9c6fa5";
+
+  constructor(address: string, privateKey?: string) {
+    this.address = address;
+    this.privateKey = privateKey ? Buffer.from(privateKey, "hex") : undefined;
+    this.web3 = new Web3(this.remoteNode);
+  }
+
+  getAddress = () => this.address;
+
+  getBalance = async () => {
+    return await this.web3.eth
+      .getBalance(this.address)
+      .then(wei => this.web3.utils.fromWei(wei, "ether"));
+  };
+
+  sendEthers = async (ethersAmount: number, receiverAddress: string) => {
+    return await this.web3.eth.sendTransaction({
+      from: this.address,
+      to: receiverAddress,
+      value: this.web3.utils.toWei(ethersAmount.toString(), "ether"),
+      gas: 21000,
+      gasPrice: this.web3.utils.toWei(this.web3.utils.toBN(10), "gwei"),
+    });
+  };
+
+  sendLocallySignedTransaction = async (
+    receiverAddress: string,
+    ethersAmount: number,
+    gasLimit: number,
+    gasPriceGwei: number
+  ) => {
+    if (!this.privateKey) {
+      console.error("Private Key not provided!");
+      return;
+    }
+
+    // Build the transaction
+    const txCount = await this.web3.eth.getTransactionCount(this.address);
+    const txData: TxData = {
+      nonce: this.web3.utils.toHex(txCount),
+      to: receiverAddress,
+      value: this.web3.utils.toHex(
+        this.web3.utils.toWei(ethersAmount.toString(), "ether")
+      ),
+      gasLimit: this.web3.utils.toHex(gasLimit),
+      gasPrice: this.web3.utils.toHex(
+        this.web3.utils.toWei(gasPriceGwei.toString(), "gwei")
+      ),
+    };
+
+    // Sign the transaction
+    const tx = new Transaction(txData, { chain: "ropsten" });
+    tx.sign(this.privateKey);
+    const serializedTx = tx.serialize();
+    const rawTx = "0x" + serializedTx.toString("hex");
+
+    // Broadcast the transaction
+    return await this.web3.eth.sendSignedTransaction(rawTx);
+  };
+}
---
+++ step25/index.ts
@@ -0,0 +1,55 @@
+import { EthereumAccount } from "./EthreumAccount";
+import dotenv from "dotenv";
+
+// Create a file named ".env" and write the environment variables as mentioned in ".env.example"
+dotenv.config({ path: "./.env" });
+
+const account1Address = process.env.ACCOUNT1_PUBLIC_KEY;
+const account2Address = process.env.ACCOUNT2_PUBLIC_KEY;
+const account1PrivateKey = process.env.ACCOUNT1_PRIVATE_KEY;
+const account2PrivateKey = process.env.ACCOUNT2_PRIVATE_KEY;
+
+if (!account1Address || !account2Address) {
+  throw new Error(
+    "Account addresses must be provided as environment variables."
+  );
+}
+
+// Wrap in a function so we can use async/await.
+(async () => {
+  // Instantiate two Ethereum account objects with the given addresses.
+  const account1Obj = new EthereumAccount(account1Address, account1PrivateKey);
+  const account2Obj = new EthereumAccount(account2Address, account2PrivateKey);
+
+  // Status of the two accounts before the transaction
+  console.log(
+    "Account 1 Balance Before Transaction:",
+    await account1Obj.getBalance()
+  );
+  console.log(
+    "Account 2 Balance Before Transaction:",
+    await account2Obj.getBalance()
+  );
+
+  console.log("Sending 1 Ether from Accout 1 to Account 2...");
+  const txData = await account1Obj.sendLocallySignedTransaction(
+    account2Obj.getAddress(),
+    1,
+    21000,
+    10
+  );
+  console.log(
+    "Transaction on EtherScan:",
+    `https://ropsten.etherscan.io/tx/${txData?.transactionHash}`
+  );
+
+  // Status of the two accounts after the transaction
+  console.log(
+    "Account 1 Balance After Transaction:",
+    await account1Obj.getBalance()
+  );
+  console.log(
+    "Account 2 Balance After Transaction:",
+    await account2Obj.getBalance()
+  );
+})();
