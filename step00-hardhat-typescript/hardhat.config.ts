e9325e33615fd1e2d357dd7afa0cc89d5e5763a4
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-10-05 16:08:21 +0500

    learn hardhat

---
+++ step00-hardhat-typescript/hardhat.config.ts
@@ -0,0 +1,47 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    ropsten: {
+      url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+    }
+  },
+  solidity: {
+    version: "0.5.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
